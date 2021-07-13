683c9951fe7745c17d8642fb9923f29a9a00fb49
ranjit0 <zodec.r1@gmail.com>
2021-07-13 14:36:39 +0545

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿
+namespace WindowsForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtFirstDigit = new System.Windows.Forms.TextBox();
+            this.txtSecondDigit = new System.Windows.Forms.TextBox();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cboOption = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(76, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "first digit";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(76, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "second digit";
+            // 
+            // txtFirstDigit
+            // 
+            this.txtFirstDigit.Location = new System.Drawing.Point(164, 39);
+            this.txtFirstDigit.Name = "txtFirstDigit";
+            this.txtFirstDigit.Size = new System.Drawing.Size(239, 22);
+            this.txtFirstDigit.TabIndex = 3;
+            // 
+            // txtSecondDigit
+            // 
+            this.txtSecondDigit.Location = new System.Drawing.Point(164, 79);
+            this.txtSecondDigit.Name = "txtSecondDigit";
+            this.txtSecondDigit.Size = new System.Drawing.Size(239, 22);
+            this.txtSecondDigit.TabIndex = 5;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(164, 163);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(108, 33);
+            this.btnCalculate.TabIndex = 7;
+            this.btnCalculate.Text = "calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cboOption);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtResult);
+            this.groupBox1.Controls.Add(this.btnCalculate);
+            this.groupBox1.Controls.Add(this.txtSecondDigit);
+            this.groupBox1.Controls.Add(this.txtFirstDigit);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(44, 27);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(529, 232);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "calculator";
+            // 
+            // cboOption
+            // 
+            this.cboOption.FormattingEnabled = true;
+            this.cboOption.Items.AddRange(new object[] {
+            "Add",
+            "Mul",
+            "Sub",
+            "Div"});
+            this.cboOption.Location = new System.Drawing.Point(164, 118);
+            this.cboOption.Name = "cboOption";
+            this.cboOption.Size = new System.Drawing.Size(121, 24);
+            this.cboOption.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(76, 125);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 17);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "option";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(164, 202);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(239, 22);
+            this.txtResult.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(608, 301);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtFirstDigit;
+        private System.Windows.Forms.TextBox txtSecondDigit;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.ComboBox cboOption;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+      
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(txtFirstDigit.Text);
+            int b = Convert.ToInt32(txtSecondDigit.Text);
+            int c = 0;
+            switch (cboOption.Text)
+            {
+                case "Add":
+                    c = a + b;
+                    break;
+                case "Sub":
+                    c = a - b;
+                    break;
+                case "Mul":
+                    c = a * b;
+                    break;
+                case "Div":
+                    c = a / b;
+                    break;
+                default:
+                    break;
+            }
+
+            txtResult.Text = c.ToString();
+                }
+
+      
+    }
+}
